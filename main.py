6f062297d8d72182dce097305bc731e4c16366ce
ishaan-rd <ishaanrd6@gmail.com>
2021-02-02 14:20:35 +0530

    request body practice

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 my_app = FastAPI()
 
@@ -7,6 +16,20 @@ async def home():
     return {"message": "Hello there!"}
 
 
+@my_app.get("/items/")
+async def get_items():
+    return {"message": "Pending work for get all items"}
+
+
 @my_app.get("/items/{item_id}")
-async def get_item_by_id(item_id):
-    return {"item-id": item_id}
+async def get_item_by_id(item_id: int):
+    return {"item-id": item_id, "message": "Pending work for get item"}
+
+
+@my_app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price * (1 + item.tax/100)
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
